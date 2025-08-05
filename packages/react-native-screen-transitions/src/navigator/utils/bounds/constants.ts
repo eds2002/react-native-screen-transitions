58f2694121dd4ad597dcf0ee260658d3cb7375b8
Eduardo Sanchez <es23jr@gmail.com>
2025-08-05 16:52:17 -0400

    refactor: whoops lol

--- packages/react-native-screen-transitions/src/navigator/utils/bounds/constants.ts
+++ packages/react-native-screen-transitions/src/navigator/utils/bounds/constants.ts
@@ -3,11 +3,14 @@ import type { MeasuredDimensions } from "react-native-reanimated";
 
 export const FULLSCREEN_DIMENSIONS = (
 	dimensions: ScaledSize,
-): MeasuredDimensions => ({
-	x: 0,
-	y: 0,
-	pageX: 0,
-	pageY: 0,
-	width: dimensions.width,
-	height: dimensions.height,
-});
+): MeasuredDimensions => {
+	"worklet";
+	return {
+		x: 0,
+		y: 0,
+		pageX: 0,
+		pageY: 0,
+		width: dimensions.width,
+		height: dimensions.height,
+	};
+};
