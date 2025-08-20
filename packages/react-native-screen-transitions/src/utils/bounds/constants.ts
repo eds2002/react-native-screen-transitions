5a3a57eb983df3195e648f0a06129ee5743e49f3
Simone Gauli <93389293+Pnlvfx@users.noreply.github.com>
2025-08-20 14:53:15 +0200

    fix: correct import paths and whitespace issues (#8)
    * Fix type import
    
    * fix: git clone fail on windows duo to an hidden whitespace on __tests__ folder
    
    * fix: 3 more imports from source found and fixed with their relative import equivalent

--- packages/react-native-screen-transitions/src/utils/bounds/constants.ts
+++ packages/react-native-screen-transitions/src/utils/bounds/constants.ts
@@ -1,28 +1,25 @@
-import type { ScaledSize } from "react-native";
-import type { MeasuredDimensions } from "react-native-reanimated";
-import type { Complete } from "src/types/utils";
-import type { BoundsBuilderOptions } from "./_types/builder";
+import type { ScaledSize } from 'react-native';
+import type { MeasuredDimensions } from 'react-native-reanimated';
+import type { Complete } from '../../types/utils';
+import type { BoundsBuilderOptions } from './_types/builder';
 
-export const FULLSCREEN_DIMENSIONS = (
-	dimensions: ScaledSize,
-): MeasuredDimensions => {
-	"worklet";
-	return {
-		x: 0,
-		y: 0,
-		pageX: 0,
-		pageY: 0,
-		width: dimensions.width,
-		height: dimensions.height,
-	};
+export const FULLSCREEN_DIMENSIONS = (dimensions: ScaledSize): MeasuredDimensions => {
+  'worklet';
+  return {
+    x: 0,
+    y: 0,
+    pageX: 0,
+    pageY: 0,
+    width: dimensions.width,
+    height: dimensions.height,
+  };
 };
 
-export const DEFAULT_BUILDER_OPTIONS: Complete<BoundsBuilderOptions> =
-	Object.freeze({
-		gestures: { x: 0, y: 0 },
-		toFullscreen: false,
-		absolute: false,
-		relative: true,
-		method: "transform",
-		contentScaleMode: "auto",
-	});
+export const DEFAULT_BUILDER_OPTIONS: Complete<BoundsBuilderOptions> = Object.freeze({
+  gestures: { x: 0, y: 0 },
+  toFullscreen: false,
+  absolute: false,
+  relative: true,
+  method: 'transform',
+  contentScaleMode: 'auto',
+});
