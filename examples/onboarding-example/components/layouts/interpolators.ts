d6ebc62c18c8e664d90238672706cd5153032d8a
Ed <ed@mac.mynetworksettings.com>
2025-12-04 11:55:30 -0500

    chore: update bound animation test

--- examples/onboarding-example/components/layouts/interpolators.ts
+++ examples/onboarding-example/components/layouts/interpolators.ts
@@ -3,12 +3,14 @@ import type { ScreenStyleInterpolator } from "react-native-screen-transitions";
 
 export const horizontalSlide: ScreenStyleInterpolator = ({
 	progress,
-	current,
 	layouts: { screen },
 	bounds,
 }) => {
 	"worklet";
 	const x = interpolate(progress, [0, 1, 2], [screen.width, 0, -screen.width]);
+	const opacity = interpolate(progress, [0, 1, 2], [0, 1, 0]);
+	const sharedBounds = bounds({ id: "SHARED" });
+
 	return {
 		contentStyle: {
 			transform: [{ translateX: x }],
