3516c71e60c0bea5973a031704cf842c227760c9
Ed <ed@mac.mynetworksettings.com>
2025-12-06 02:30:15 -0500

    chore: improve tray routes

--- examples/tray/app/_layout.tsx
+++ examples/tray/app/_layout.tsx
@@ -2,52 +2,42 @@ import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { BlankStack } from "@/components/layouts/blank-stack";
 import "react-native-reanimated";
+import { interpolate } from "react-native-reanimated";
 import Transition from "react-native-screen-transitions";
-import { Tray } from "@/components/tray";
 
 export default function RootLayout() {
 	return (
 		<ThemeProvider value={DefaultTheme}>
 			<BlankStack>
 				<BlankStack.Screen
-					name="tray-routes/index"
+					name="tray-routes"
 					options={{
 						gestureDirection: "vertical",
 						gestureEnabled: true,
 						screenStyleInterpolator: (props) => {
 							"worklet";
-
-							const trayStyles = Tray.interpolator();
-							return trayStyles(props);
-						},
-						transitionSpec: {
-							open: Transition.Specs.DefaultSpec,
-							close: Transition.Specs.DefaultSpec,
-						},
-					}}
-				/>
-				<BlankStack.Screen
-					name="tray-routes/b"
-					options={{
-						screenStyleInterpolator: (props) => {
-							"worklet";
-
-							const trayStyles = Tray.interpolator();
-							return trayStyles(props);
-						},
-						transitionSpec: {
-							open: Transition.Specs.DefaultSpec,
-							close: Transition.Specs.DefaultSpec,
-						},
-					}}
-				/>
-				<BlankStack.Screen
-					name="tray-routes/c"
-					options={{
-						screenStyleInterpolator: (props) => {
-							"worklet";
-							const trayStyles = Tray.interpolator();
-							return trayStyles(props);
+							if (props.focused) {
+								const y = interpolate(
+									props.progress,
+									[0, 1],
+									[props.layouts.screen.height, 0],
+								);
+								const overlayOpacity = interpolate(
+									props.progress,
+									[0, 1],
+									[0, 1],
+								);
+								return {
+									contentStyle: {
+										transform: [{ translateY: y }],
+									},
+									overlayStyle: {
+										opacity: overlayOpacity,
+										backgroundColor: "rgba(0, 0, 0, 0.5)",
+									},
+								};
+							}
+							return {};
 						},
 						transitionSpec: {
 							open: Transition.Specs.DefaultSpec,
