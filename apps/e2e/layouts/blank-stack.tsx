dd08ff0c705852c6b9cd4568008ade4a4d2acadd
ed <es23jr@gmail.com>
2026-03-26 22:01:19 -0400

    refactor(e2e): fix stack

--- apps/e2e/layouts/blank-stack.tsx
+++ apps/e2e/layouts/blank-stack.tsx
@@ -4,6 +4,7 @@ import type {
 	StackNavigationState,
 } from "@react-navigation/native";
 import { withLayoutContext } from "expo-router";
+import type { ComponentProps } from "react";
 import { Platform } from "react-native";
 import {
 	type BlankStackNavigationEventMap,
@@ -11,19 +12,25 @@ import {
 	createBlankStackNavigator,
 } from "react-native-screen-transitions/blank-stack";
 
-const { Navigator } = createBlankStackNavigator({
+const { Navigator } = createBlankStackNavigator();
+
+function BlankStackNavigator(props: ComponentProps<typeof Navigator>) {
 	/**
-	 * Note, on android you may use native screens, however, pass through events seem to be a little buggy.
-	 * During fast navigation, or even deeply nested navigational events on android, you do get a Fragment Manager
-	 * bug. Hoping this could possibly be solved in the next major release of react-native-screens. Or maybe this is an issue on our
-	 * end LOL
+	 * Note, on android you may use native screens, however, pass through
+	 * events seem to be a little buggy. During fast navigation, or even deeply
+	 * nested navigational events on android, you do get a Fragment Manager bug.
+	 * Hoping this could possibly be solved in the next major release of
+	 * react-native-screens. Or maybe this is an issue on our end LOL
 	 */
-	enableNativeScreens: Platform.OS === "ios",
-});
+	const enableNativeScreens =
+		props.enableNativeScreens ?? Platform.OS === "ios";
+
+	return <Navigator {...props} enableNativeScreens={enableNativeScreens} />;
+}
 
 export const BlankStack = withLayoutContext<
 	BlankStackNavigationOptions,
-	typeof Navigator,
+	typeof BlankStackNavigator,
 	StackNavigationState<ParamListBase>,
 	BlankStackNavigationEventMap
->(Navigator);
+>(BlankStackNavigator);
