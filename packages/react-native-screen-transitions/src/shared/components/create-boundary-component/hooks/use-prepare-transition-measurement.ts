2dcceece5ba3227ed86ef8c20b1a295c32c8dd7e
ed <es23jr@gmail.com>
2026-03-28 22:45:39 -0400

    fix(bounds): avoid measuring while still in flight

--- packages/react-native-screen-transitions/src/shared/components/create-boundary-component/hooks/use-prepare-transition-measurement.ts
+++ packages/react-native-screen-transitions/src/shared/components/create-boundary-component/hooks/use-prepare-transition-measurement.ts
@@ -1,6 +1,7 @@
 import { useAnimatedReaction } from "react-native-reanimated";
 import { AnimationStore } from "../../../stores/animation.store";
 import { BoundStore } from "../../../stores/bounds";
+import { GestureStore } from "../../../stores/gesture.store";
 import type { BoundaryId, MaybeMeasureAndStoreParams } from "../types";
 import {
 	PREPARE_DESTINATION_MEASUREMENT_INTENT,
@@ -32,8 +33,19 @@ export const usePrepareTransitionMeasurement = (params: {
 		currentScreenKey,
 		"willAnimate",
 	);
+	const currentAnimating = AnimationStore.getValue(
+		currentScreenKey,
+		"animating",
+	);
+	const currentDragging = GestureStore.getValue(currentScreenKey, "dragging");
 	const nextWillAnimate = nextScreenKey
 		? AnimationStore.getValue(nextScreenKey, "willAnimate")
+		: null;
+	const nextAnimating = nextScreenKey
+		? AnimationStore.getValue(nextScreenKey, "animating")
+		: null;
+	const nextDragging = nextScreenKey
+		? GestureStore.getValue(nextScreenKey, "dragging")
 		: null;
 
 	useAnimatedReaction(
@@ -47,7 +59,13 @@ export const usePrepareTransitionMeasurement = (params: {
 				? BoundStore.getGroupActiveId(group)
 				: null;
 
-			if (currentGroupActiveId !== String(id)) {
+			if (group && currentGroupActiveId !== String(id)) {
+				return;
+			}
+
+			const shouldCancelMeasurement =
+				!!nextAnimating?.get() && !!nextDragging?.get();
+			if (shouldCancelMeasurement) {
 				return;
 			}
 
@@ -71,7 +89,11 @@ export const usePrepareTransitionMeasurement = (params: {
 			const currentGroupActiveId = group
 				? BoundStore.getGroupActiveId(group)
 				: null;
-			if (currentGroupActiveId !== String(id)) return;
+			if (group && currentGroupActiveId !== String(id)) return;
+
+			const shouldCancelMeasurement =
+				!!currentAnimating.get() && !!currentDragging.get();
+			if (shouldCancelMeasurement) return;
 
 			maybeMeasureAndStore({
 				intent: PREPARE_DESTINATION_MEASUREMENT_INTENT,
